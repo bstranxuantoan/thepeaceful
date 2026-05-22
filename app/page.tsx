440e0efd5ad369d5cf39868a36c807dcb57782ef
Tran Xuan Toan <bstranxuantoa@gmail.com>
2026-05-22 18:01:08 +0700

    fix: restore missing Image import in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import LeadForm from '@/components/LeadForm'
 import FAQAccordion from '@/components/FAQAccordion'
 import StickyMobileCTA from '@/components/StickyMobileCTA'
